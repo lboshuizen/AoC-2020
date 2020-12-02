dc755d8cfc06a7b443de1641993001676d0a095a
Leon Boshuizen <llc.boshuizen@gmail.com>
2020-12-02 13:42:59 +0100

    day2

---
+++ src/Day2/PasswordPhilosophy.hs
@@ -0,0 +1,24 @@
+module Day2.PasswordPhilosophy (passwordPhilosophy) where
+
+import Data.List
+import Utils (stoi,split)
+
+policy1 :: (Int,Int,Char,String) -> Bool
+policy1 (p1,p2,c,pwd) = cnt >= p1 && cnt <= p2
+    where cnt = length (filter (==c) pwd)
+
+policy2 :: (Int,Int,Char,String) -> Bool
+policy2 (p1,p2,c,pwd) 
+    | length pwd < p2 = False
+    | otherwise = (at p1) /= (at p2)
+        where at n = pwd !! (n-1) == c
+
+decode :: String -> (Int,Int,Char,String)
+decode s = (p1,p2,c,pwd)
+    where (minmax:cc:pwd:_) = words s
+          (p1:p2:_)= (map stoi . split '-') minmax
+          c = head cc
+
+conform p = p . decode
+
+passwordPhilosophy = filter (conform policy2)
--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,10 @@
 module Main where
 
 import Utils
-import Day1.ReportRepair
+import Day2.PasswordPhilosophy
 
 main :: IO ()
 main = do
-  r <- readLines "./data/day1.txt"
-  let xs = map stoi r
-  let r = reportRepair 2020 xs  
-  print r
+  r <- readLines "./data/day2.txt"
+  let l = length (passwordPhilosophy r)
+  print l
