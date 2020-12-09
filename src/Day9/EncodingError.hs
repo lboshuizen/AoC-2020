a4d20beb9efacee1b1c990ac6944a25260da8326
Leon Boshuizen <llc.boshuizen@gmail.com>
2020-12-09 12:57:09 +0100

    part2
    refactored day9
    
    ref

--- src/Day9/EncodingError.hs
+++ src/Day9/EncodingError.hs
@@ -1,21 +1,25 @@
 module Day9.EncodingError (encoding) where
 
-import Data.List (nub)
+import Data.List (inits, tails)
 import Utils (stoi)
 
-sums :: (Eq a, Num a) => [a] -> [a]
-sums xs = nub $ [ (l+r) | l <- xs, r <- xs, l /= r ]
-
 windows :: Int -> [a] -> [[a]]
-windows n xs
-    | (length xs) < n = []
-    | otherwise = take n xs : windows n (drop 1 xs)
+windows n xs = [ take n . drop i $ xs | i <- [0..(length xs) - n] ]
 
-isSumOfPred :: [Int] -> Bool
-isSumOfPred xs = elem t (sums l)
-    where (t:l) = reverse $ xs
+lastIsSumOfPred :: [Int] -> Bool
+lastIsSumOfPred = canSum . reverse
+    where canSum (x:xs) = or [ l+r==x | l <- xs, r <- xs, l /= r ]
 
-part1 = last . head . dropWhile isSumOfPred . windows 26 . map stoi
+part1 = last . head . dropWhile lastIsSumOfPred . windows (25+1)
+
+sweep :: Int -> [Int] -> [[Int]]
+sweep n = dropWhile (\l -> sum l /= n) . concatMap tails . inits
+
+part2 xs = min + max
+    where
+        n = part1 xs
+        seq = head . sweep n $ xs
+        (min,max) = (minimum seq, maximum seq)
 
 encoding :: [String] -> Int
-encoding = last . head . dropWhile isSumOfPred . windows 26 . map stoi
+encoding = part2 . map stoi
