892809466f8d840d73e7fbf8ff9718c386597472
Leon Boshuizen <llc.boshuizen@gmail.com>
2020-12-14 19:01:01 +0100

    refactorings day14 60ms -> 23ms

--- src/Day14/DockingData.hs
+++ src/Day14/DockingData.hs
@@ -1,77 +1,76 @@
 module Day14.DockingData (docking) where
 
 import Data.Bifunctor (second)
-import Data.Bits
-import qualified Data.HashMap.Strict as M
+import Data.Bits (clearBit, setBit)
+import qualified Data.IntMap.Strict as M
 import Data.List (foldl', isPrefixOf, stripPrefix)
 import Data.List.Split (splitOn)
 import Data.Maybe (fromJust)
-import Utils (stoi)
 
 data TriBit = H | L | F deriving (Eq, Show)
 
+type Mask = [(Int, TriBit)]
+
+type Strategy = Mask -> [Ins] -> [Ins]
+
 data Ins
-  = Mask [(Int, TriBit)]
-  | Write Integer Integer
+  = MaskIns [(Int, TriBit)]
+  | Write Int Int
   deriving (Eq, Show)
 
 parse :: String -> Ins
 parse s
-  | "mask" `isPrefixOf` s = Mask . zipWith (curry (second ctoi)) [0 ..] . reverse . (!! 1) . splitOn " = " $ s
+  | "mask" `isPrefixOf` s = MaskIns . zipWith (curry (second bit)) [0 ..] . reverse . (!! 1) . splitOn " = " $ s
   | otherwise = asWrite . map read . splitOn "] =" . fromJust . stripPrefix "mem[" $ s
   where
     asWrite (a : v : _) = Write a v
-    ctoi '1' = H
-    ctoi '0' = L
-    ctoi 'X' = F
+    bit '1' = H
+    bit '0' = L
+    bit 'X' = F
 
-writeBit :: (Int, TriBit) -> Integer -> Integer
+writeBit :: (Int, TriBit) -> Int -> Int
 writeBit (n, L) i = clearBit i n
 writeBit (n, H) i = setBit i n
 writeBit _ i = i
 
-applyMask :: Ins -> Integer -> Integer
-applyMask (Mask m) n = foldr writeBit n m
+applyMask :: Mask -> Int -> Int
+applyMask m n = foldr writeBit n m
 
 isMask :: Ins -> Bool
-isMask (Mask _) = True
+isMask (MaskIns _) = True
 isMask _ = False
 
-decoderV1 :: [Ins] -> [Ins]
-decoderV1 [] = []
-decoderV1 (Mask m : xs) = v ++ decoderV1 r
-  where
-    (h, r) = break isMask xs
-    v = map (\(Write a v) -> Write a (applyMask (Mask m) v)) h
-decoderV1 _ = error "group should start with as mask ins"
-
-expndAdr :: [(Int, TriBit)] -> Integer -> [Integer]
+expndAdr :: Mask -> Int -> [Int]
 expndAdr [] i = [i]
 expndAdr ((n, F) : xs) i = expndAdr xs (clearBit i n) ++ expndAdr xs (setBit i n)
 expndAdr ((n, H) : xs) i = expndAdr xs (setBit i n)
 expndAdr (_ : xs) i = expndAdr xs i
 
-writes :: [(Int, TriBit)] -> Ins -> [Ins]
-writes m (Write a v) = map (`Write` v) $ expndAdr m a
-
-decoderV2 :: [Ins] -> [Ins]
-decoderV2 [] = []
-decoderV2 (Mask m : xs) = v ++ decoderV2 t
+v1, v2 :: Strategy
+v1 m = map (\(Write a v) -> Write a (applyMask m v))
+v2 m = concatMap (writes m)
   where
-    (h, t) = break isMask xs
-    v = concatMap (writes m) h
-decoderV2 _ = error "group should start with as mask ins"
+    writes m (Write a v) = map (`Write` v) $ expndAdr m a
 
-collect :: [Ins] -> Integer
-collect = sum . M.elems . foldl' (\m (Write a v) -> M.insert a v m) M.empty
+decoder :: Strategy -> [Ins] -> [Ins]
+decoder _ [] = []
+decoder f (MaskIns m : xs) = v ++ decoder f r
+  where
+    (h, r) = break isMask xs
+    v = f m h
+decoder _ _ = error "group should start with as mask ins"
 
-part1, part2 :: [Ins] -> Integer
-part1 = collect . decoderV1
-part2 = collect . decoderV2
+collect :: [Ins] -> Int
+collect = sumMap . toMap
+  where
+    toMap = M.fromList . map (\(Write a v) -> (a, v))
+    sumMap = M.foldr' (+) 0
 
-docking :: [String] -> (Integer, Integer)
-docking xs = (a, b)
+part1, part2 :: [Ins] -> Int
+part1 = collect . decoder v1
+part2 = collect . decoder v2
+
+docking :: [String] -> (Int, Int)
+docking xs = (part1 p, part2 p)
   where
     p = map parse xs
-    a = part1 p
-    b = part2 p
